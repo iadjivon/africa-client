3e8d30fda9b8f421862f6d19d9254e225fb6c007
Ida Adjivon <65119712+iadjivon@users.noreply.github.com>
2020-12-19 04:04:38 -0500

    map renders in react. working on rendering text

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Display from './components/visualization';
+import data from './components/africa.geo.json';
 import './App.css';
 
 function App() {
+  const [property, setProperty] = useState("pop_est");
+  // const svgRef = useRef();
+  // useEffect(() => {
+  //   const svg = select(svgRef.current)
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+  <h1> Africa map</h1>
+  <React.Fragment>
+       <Display data={data} property={property}/> 
+       <select
+        value={property}
+        onChange={event => setProperty(event.target.value)}
+      >
+        <option value="pop_est">Population</option>
+        <option value="name_len">Name length</option>
+        <option value="gdp_md_est">GDP</option>
+      </select>
+      </React.Fragment>
+</>)
+  
 }
 
 export default App;
---
+++ src/components/visualization.js
@@ -0,0 +1,70 @@
+import React, {useRef, useEffect, useState} from 'react';
+import {select, geoPath, geoMercator, path, svg }from 'd3'
+
+import data from './africa.geo.json'
+import * as d3 from 'd3'
+
+
+
+const Display = ({data, property}) =>{
+
+    const svgRef = useRef();
+    // const wrapperRef = useRef();
+
+    const width = 500;
+    const height = 500;
+
+    const [selectedCountry, setSelectedCountry] = useState(null);
+    
+const features = 
+
+
+
+useEffect(() => {
+    const svg = select(svgRef.current);
+    const projection = geoMercator()
+    // .scale(400)
+    // .center("center")
+    // .translate([width/2, height/5])
+    .fitSize([height, width], 
+        selectedCountry || data)
+    .precision(100);
+
+
+const path = d3.geoPath(projection);
+
+// render each country 
+svg.selectAll(".country")
+    .data(data.features)
+    .join("path")
+    .on("click", feature => {
+        setSelectedCountry(selectedCountry === feature ? null : feature);
+    })
+    .attr("class", "country")
+    .transition()
+    .attr("d", feature => path(feature));
+    // .attr("fill", feature.properties[property])
+    // .attr("preserveAspectRatio", "xMinYMin");
+
+// render text
+svg.selectAll(".label")
+    .data([selectedCountry])
+    .join("text")
+    .attr("class", "label")
+    .text(feature => feature && features.properties.name)
+
+ }, [data, property, selectedCountry]);
+
+
+
+
+return (
+<div class ="map" style={{ padding: "2rem" }}>
+    <svg ref={svgRef}></svg>
+</div>
+
+)
+    
+}
+
+export default Display;
