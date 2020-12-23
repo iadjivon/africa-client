db07eaeabb6d0072ed56d1c99f67ae3a1ef18e56
Ida Adjivon <idaadjivon@idas-mbp-2.myfiosgateway.com>
2020-12-23 02:53:09 -0500

    African map displays and buttons navigate between pages

--- src/App.js
+++ src/App.js
@@ -1,30 +1,102 @@
 import React, {useState} from 'react';
-import Display from './components/visualization';
+import Display from './components/Visualization';
 import data from './components/africa.geo.json';
 import './App.css';
+import { Route, Link, Switch } from "react-router-dom";
+import Recipes from './components/Recipe'
+
 
 function App() {
-  const [property, setProperty] = useState("pop_est");
+
+//Variable to hold url 
+   const url = "http://localhost:3000"
+  const [property, setProperty] = React.useState([]);
+
+// state to hold recipes 
+const [recipes, setRecipes] = React.useState([])
+
+// Empty Recipes
+
+const emptyRecipe = {
+  name: "",
+  cookTime: 0,
+  description: "",
+  servingSize: 0,
+  ingredients: "",
+  instructions: "", 
+  recipesCompleted: Boolean
+
+}
+
+// selected state, this will represent the edited recipe
+const [selectedRecipe, setSelectedRecipe] = React.useState(emptyRecipe)
+// Function to get recipes via API
+const getRecipes = () =>{
+  fetch(url + "/africa/")
+  .then(response => response.json()
+  .then(data => {
+    setRecipes(data)
+  }))
+}
+// useEffect to do initial call of getRecipes
+React.useEffect(()=>{
+  getRecipes()
+}, [])
+
+
+
+
+
+
   // const svgRef = useRef();
   // useEffect(() => {
   //   const svg = select(svgRef.current)
   // }, []);
 
+
+
+
+
+
+
+
+
+
+
+
+  
+
   return (
-  <>
-  <h1> Africa map</h1>
-  <React.Fragment>
-       <Display data={data} property={property}/> 
-       <select
-        value={property}
-        onChange={event => setProperty(event.target.value)}
-      >
-        <option value="pop_est">Population</option>
-        <option value="name_len">Name length</option>
-        <option value="gdp_md_est">GDP</option>
-      </select>
-      </React.Fragment>
-</>)
+  <div>
+  <h1> African Recipes </h1>
+  <hr />
+  <Link to="/">
+        <button>Home </button>
+  </Link>
+  <Link to="/recipes">
+        <button>Explore Recipes </button>
+  </Link>
+
+
+  <main> 
+  <Switch>
+        <Route exact path ="/" render={ () => 
+          <Display data={data} property={property}/> }/>
+
+        <Route
+            exact
+            path="/recipes"
+            render={(rp) => (
+              <Recipes recipes= {recipes} {...rp} label="create" recipe={{emptyRecipe}} />
+            )}
+          />  
+
+
+
+</Switch>
+      </main>
+
+</div>)
   
 }
 
---
+++ src/components/Recipe.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Recipe = (props) =>{
+    const {recipes} = props
+
+    const loaded = () =>(
+        <div style={{textAlign: "center"}}>
+          {recipes.map((recipe) =>(
+            <article>
+              <h1>{recipe.name}</h1>
+              <h3>{recipe.servingSize}</h3>
+              <h3>{recipe.description}</h3>
+              <h3>{recipe.cookTime}</h3>
+              <h3>{recipe.ingredients}</h3>
+              <h3>{recipe.instructions}</h3>
+
+              <button onClick={()=>{props.selectRecipe(recipe);
+              props.history.push("/edit")}}> Edit</button>
+      
+              <button onClick={()=>{props.deleteRecipe(recipe);
+            }}> Delete </button>
+            </article>))}
+        </div>
+      )
+
+      const loading = <h1> Loading ... </h1>
+
+    return recipes.length > 0 ? loaded() : loading
+};
+
+export default Recipe;
